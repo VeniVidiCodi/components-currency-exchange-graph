e92d476db3102fff3f662ad0d7b9faf46176cba5
Victor Sanchez <incomingvrs@gmail.com>
2020-01-25 21:17:28 -0800

    transfer static chart HTML and CSS

--- src/App.js
+++ src/App.js
@@ -4,21 +4,27 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+        <div class="navBar">
+            <h1>International Currency Rates</h1>
+        </div>
+        <div class="margin-left"></div>
+        
+
+        <div class="chart-container">
+            <div class="chart-topSection">
+              
+            </div>
+            <div class="chart-content">
+                <div class="chart-bottomSection">
+                </div>
+            </div>
+
+        </div> 
+
+
+        <div class="margin-right"></div>
+        
     </div>
   );
 }

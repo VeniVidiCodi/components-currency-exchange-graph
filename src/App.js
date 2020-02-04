7474d5cb04f735abcb639cbf884a4e9366a339cf
Victor Sanchez <incomingvrs@gmail.com>
2020-02-04 11:00:16 -0800

    bar rendering funtional, state arrays update functionality

--- src/App.js
+++ src/App.js
@@ -9,23 +9,18 @@ class App extends Component {
         base: "USD",
         rates: [],
         chosenKeys: [], //['HKD', 'USD'']
-        chosenRates: [] // ['1.7468', '98969']
+        chosenRates: [], // ['1.7468', '98969']
+        height: 1
     };
 // ==========================================================================
+
     // Define methods. Must have for forms.
     selectBase = (ev) => {
-        // console.log('1');
-        // console.log('EV.TARGET.VALUE', ev.target.value);
         this.setState({ base: ev.target.value }, this.updateInfo);
-
-        // console.log("Selecting New Base Currency...");
-        // console.log('base at select', this.state.base);
     }
 
     updateInfo = () => {
-        // console.log('BASE', this.state.base);
         this.onRefresh();
-        // console.log(this.state.rates);
     }
 
     selectRate = (ev) => {
@@ -36,145 +31,11 @@ class App extends Component {
         let chosenRate = this.state.rates[choice];
         let chosenRatesUpdate = this.state.chosenRates.concat(chosenRate);
         this.setState({ chosenRates: chosenRatesUpdate });
-            
-        // console.log("FilterCHECK", chosenKeys.filter(key => (key of this.state.rates) ?)
-        // let values = Object.values(rates);
-        // let chosenValues = this.state.chosenRates.concat(values);
-        // this.setState({ chosenRates: chosenValues });
-
-        // {
-        //     (key === (rate in rates)) ? (
-        //         chosenRates().concat(rate.values)
-        //         ): null; 
-        // }
-        // this.checkRateList();
-        // this.makeBar();
-        // this.rateBar();
     }
 
-    // rateBar = () => {
-    //     const rates = this.state.rates;
-    //     const chosenRates = this.state.chosenRates;
-    //     const chosenKeys = this.state.chosenKeys;
+    fetchUpdateChosenRates = () => {
         
-    //     rates.includes(rateList) ? (
-    //     this.state.rates.map((currency) => (
-    //         <div className="bar" style={{height: currency + "%"}}>
-    //         {currency}
-    //          </div>
-    //     ))): null
-        
-
-    
-        // Object.keys(this.state.chosenRates).map(rate,  => ( 
-        //     <div className="bar" style={{height: currency.percentage + "%"}}>
-        //     {}
-        //     </div>
-    //     // ))
-    // }
-
-
-
-    // makeBar = () => {
-        // console.log("Cont...", this.state.rates);
-        // const rateKeys = Object.keys(this.state.rates);
-        // console.log('rateKeys:::', rateKeys);
-        // const rateListKeys = chosenRates.map((rateKeys) =>(
-            
-        // ));
-        // console.log('rateListKeys:::', rateListKeys);
-
-
-        // (rates.filter(key => this.state.rates[key].main == true)) ? {
-
-        
-        // rates.map((code, rate) => (
-        //     (rate in this.state.chosenRates) ? (
-        //         <div>
-        //             {rate}
-        //         </div>
-        //     ): (
-        //         null
-        //     )
-        // ))
-    // }
-
-    // checkRateList = () => {
-    //     // if new value in chosen keys is in rates array {
-    //     //     then push key value pair to chosenRates
-    //     // }
-    //     console.log("checkRateList", this.state.rates);
-    //     console.log("checkChosenKeys", this.state.chosenKeys);
-    //     console.log("checkChosenRates", this.state.chosenRates);
-    // }
-
-
-
-    // recordItem = () => {
-    //     console.log('-------- recordItem');
-
-    //     // Fetch the inputs from the page (and console.log for debugging)
-    //     let currencyItem = this.state.rates(item, value);
-    //     let currencyCode = currencyItem[item];
-    //     let currencyValue = currencyItem[value];
-    //     console.log('Currency Inputs:', currencyCode, currencyValue);
-
-    //     // Get the value of the inputs (and console.log for debugging)
-    //     let name = nameInput.value;
-    //     let powerLevel = powerInput.value;
-    //     console.log('Values:', name, powerLevel);
-
-    //     // Challenge 4:
-    //     if (name === '') {
-    //         return; // End the function, prevent an empty input
-    //     }
-
-    //     // Push the new info onto the array & rerender
-    //     let pair = [powerLevel, name];
-    //     powerLevels.push(pair);
-    //     render();
-
-    //     // Challenge 4:
-    //     powerInput.value = '';
-    //     nameInput.value = '';
-    // }
-
-
-    // Renders bars in chart
-    addRateBar = (ev) => {
-        // this.setState.chosenRates().push()
-        console.log('2');
-        const value = ev.target.value;
-        this.setState.ratesList({
-            rate: value,
-        });
     }
-
-    // removeRateBar = (ev) => {
-    //     console.log('3');
-    //     console.log("Testing RemoveBar");
-    //     // return chosenRates List without selcted value (bar clicked)
-    //     this.setState({})
-    // }
-    removeRate = (array, rate) => {
-    // if the rate is in the array, slice it 
-        const chosenRates = array.slice(); // duplicating array
-        const newChosenRates = chosenRates.filter((rate) => rate !== array);
-        return newChosenRates;
-    }
-
-    removeBar = (ev) => {
-        // when displayed bar is clicked, 
-        // remove that bar from ratesList
-        // refresh the page 
-    }
-
-//    barHeight = () => {
-//     //    TODO here
-//     }
-
-
-
 
 // ==========================================================================
 
@@ -194,12 +55,20 @@ class App extends Component {
             .then(response => response.json())
             .then(data => {
                 console.log('receiving data', data);
+                // Transfer/Update of Keys
+                let newChosenRates = [];
+                for ( const item of this.state.chosenKeys ){
+                    console.log('item:', item, 'value', this.state.rates[item]);
+                    newChosenRates.push(this.state.rates[item])
+                }
+
 
                 // Do state set for real data
                 this.setState({
-                    base: data.base,
+                    // base: data.base,
                     date: data.date,
                     rates: data.rates,
+                    chosenRates: newChosenRates,
                 });              
             });
     }
@@ -209,12 +78,14 @@ class App extends Component {
 
     render() {
         // console.log('6');
+        const chosenRates = this.state.chosenRates;
         console.log("rendering page...");
-        console.log("chosen keys: ", this.state.chosenKeys);
         // console.log(this.state.rates["HKD"]);
         console.log("rates: ", this.state.rates);
         console.log("base: ", this.state.base);
+        console.log("chosen keys: ", this.state.chosenKeys);
         console.log("chosenRates: ", this.state.chosenRates);
+
 
         return (
             <div className="container">
@@ -250,23 +121,19 @@ class App extends Component {
                                 </form>
                             </div>
                             <div className="chart-content">
-                                    {/* {rateList} */}
+                                    {/* <div className="BarChart"> */}
+                                        { this.state.chosenKeys.map((currency, index) => (
+                                        <div className="bar" style={{height: this.state.chosenRates[index] + "%"}}>
+                                        {currency}
+                                        </div>))}
 
-                                    <div className="BarChart">
-                                        {this.rateBar}
-                                    </div>
-
-                                    {/* {
-                                        this.state.chatLog.map(text => (
-                                            <p>Message: {text}</p>
-                                        ))
-                                    }
-                                    <input onChange={this.onMessageChange}
-                                        value={this.state.message} />
-                                    <button onClick={() => alert("Hi")}>
-                                        <img src={sendIcon} />
-                                        Send message
-                                    </button> */}
+                                        {/* {Object.keys(this.state.chosenKeys).map(currency => (
+                                        <div className="bar" style={{height: currency + "%"}}>
+                                            {currency}
+                                                </div>
+                                            ))
+                                            } */}
+                                    {/* </div> */}
 
                                     {/* <div 
                                         onclick="alert('EUR costs 0.88 pounds')"
